4c9e653825161bb0b8debc4684a1276291e8eddd
nilaisr <nilaisr@gmail.com>
2019-07-09 10:26:37 +0200

    add kernel view

--- methods/two_streams_rgb.py
+++ methods/two_streams_rgb.py
@@ -8,6 +8,8 @@ from model import two_streams
 from methods import rgb2pca
 from dataset import *
 
+import numpy as np
+
 
 def two_streams_rgb():
     dataset_train = '/home/cic/datasets/ImageNet/train/'
@@ -15,39 +17,46 @@ def two_streams_rgb():
     save_dir = '/home/nsallent/output/saved_models/'
     model_name = 'two_streams_rgb'
 
-    x_train_all = []
-    y_train_all = []
-
-    for folder in listdir(dataset_train):
-        y_train_all.extend([folder] * len(listdir(dataset_train + folder)))
-        x_train_all.extend([cv2.imread(dataset_train + folder + '/' + im) for im in listdir(dataset_train + folder)])
+    # for folder in listdir(dataset_train):
+    #     y_train_all.extend([folder] * len(listdir(dataset_train + folder)))
+    #     x_train_all.extend([cv2.imread(dataset_train + folder + '/' + im) for im in listdir(dataset_train + folder)])
 
     x_train = []
     y_train = []
 
-    for value in classes_values:
-        for im, label in zip(x_train_all, y_train_all):
-            if label == value:
-                im_pca = rgb2pca(im)
+    for folder in listdir(dataset_train):
+        if folder in classes_values:
+            for im in listdir(dataset_train + folder):
+                im_pca = rgb2pca(cv2.imread(dataset_train + folder + '/' + im))
                 x_train.append(im_pca)
-                y_train.append(label)
+                y_train.append(folder)
 
-    x_test_all = []
-    y_test_all = []
-
-    for folder in listdir(dataset_test):
-        y_test_all.extend([folder] * len(listdir(dataset_test + folder)))
-        x_test_all.extend([cv2.imread(dataset_test + folder + '/' + im) for im in listdir(dataset_test + folder)])
+    print(np.unique(y_train))
 
     x_test = []
     y_test = []
 
-    for value in classes_values:
-        for im, label in zip(x_test_all, y_test_all):
-            if label == value:
-                im_pca = rgb2pca(im)
+    for folder in listdir(dataset_test):
+        if folder in classes_values:
+            for im in listdir(dataset_test + folder):
+                im_pca = rgb2pca(cv2.imread(dataset_test + folder + '/' + im))
                 x_test.append(im_pca)
-                y_test.append(label)
+                y_test.append(folder)
+
+    print(np.unique(y_test))
+
+    # y_test_all.extend([folder] * len(listdir(dataset_test + folder)))
+    # x_test_all.extend([cv2.imread(dataset_test + folder + '/' + im) for im in listdir(dataset_test + folder)])
+    #
+    # x_test = []
+    # y_test = []
+    #
+    # for value in classes_values:
+    #     for im, label in zip(x_test_all, y_test_all):
+    #         if label == value:
+    #             im_pca = rgb2pca(im)
+    #             x_test.append(im_pca)
+    #             y_test.append(label)
 
     print('All images loaded.')
 
