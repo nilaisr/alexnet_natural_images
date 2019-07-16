8a7a96b22df4d405b81f05d74288b2d2d1de6310
nilaisr <nilaisr@gmail.com>
2019-07-16 09:17:21 +0200

    fix

--- model/two_streams.py
+++ model/two_streams.py
@@ -23,16 +23,12 @@ def conv2D_bn(x, filters, num_row, num_col, strides=(1, 1), padding='same', name
     Returns:
         Output tensor after applying `Conv2D` and `BatchNormalization`.
     """
-    if name is not None:
-        bn_name = name + '_bn'
-        conv_name = name + '_conv'
-    else:
-        bn_name = None
-        conv_name = None
-    if K.image_data_format() == 'channels_first':
-        bn_axis = 1
-    else:
-        bn_axis = 3
+    bn_name = None
+    conv_name = None
+    # if K.image_data_format() == 'channels_first':
+    bn_axis = 1
+    # else:
+    #     bn_axis = 3
     x = Conv2D(filters, (num_row, num_col),
                strides=strides, padding=padding,
                use_bias=False, name=conv_name)(x)
