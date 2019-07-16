f605c5235073ae287896375dfa238ad56c1a4d34
nilaisr <nilaisr@gmail.com>
2019-07-16 12:18:19 +0200

    mod

--- model/two_streams.py
+++ model/two_streams.py
@@ -100,13 +100,13 @@ def two_streams():
     y3 = ZeroPadding2D(padding=(1, 1), data_format=data_format)(y3)
 
     # Channel 1 - Cov Net Layer 7
-    x4 = Multiply(axis=concat_axis)([x3, y3])
+    x4 = Multiply()([x3, y3])
     x4 = Flatten()(x4)
     x4 = Dense(2048, activation='relu')(x4)
     x4 = Dropout(dropout)(x4)
 
     # Channel 2 - Cov Net Layer 7
-    y4 = Multiply(axis=concat_axis)([x3, y3])
+    y4 = Multiply()([x3, y3])
     y4 = Flatten()(y4)
     y4 = Dense(2048, activation='relu')(y4)
     y4 = Dropout(dropout)(y4)
