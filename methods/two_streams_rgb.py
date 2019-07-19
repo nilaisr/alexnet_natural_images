26e20aa6cc289b405b03dad8af159c9378607f10
nilaisr <nilaisr@gmail.com>
2019-07-19 09:31:09 +0200

    put preprocess

--- methods/two_streams_rgb.py
+++ methods/two_streams_rgb.py
@@ -40,14 +40,16 @@ def two_streams_rgb():
     train_generator = train_datagen.flow_from_directory(
         dataset_train,
         target_size=(input_size, input_size),
-        classes=classes_train[:3],
-        class_mode='categorical')
+        classes=classes_train[:3])
+
+    train_generator[0] = [rgb2pca(x) for x in train_generator[0]]
 
     validation_generator = test_datagen.flow_from_directory(
         dataset_test,
         target_size=(input_size, input_size),
-        classes=classes_test[:3],
-        class_mode='categorical')
+        classes=classes_test[:3])
+
+    validation_generator[0] = [rgb2pca(x) for x in validation_generator[0]]
 
     output, im_input, input_shape = two_streams()
 
