9322ae9205f707acc12f77adce29c60ee373afb3
nilaisr <nilaisr@gmail.com>
2019-07-19 10:51:46 +0200

    test preprocess

--- methods/two_streams_rgb.py
+++ methods/two_streams_rgb.py
@@ -37,7 +37,7 @@ def two_streams_rgb():
         image = np.array(image)
         print(type(image), image.shape, np.amax(image), np.amin(image))
         pca_image = rgb2pca(image)
-        pca_image = minmax_scale(pca_image, (1,255))
+        pca_image = minmax_scale(pca_image, (1, 255))
         print(type(pca_image), pca_image.shape, np.amax(pca_image), np.amin(pca_image))
         hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
         print(type(hsv_image), hsv_image.shape, np.amax(hsv_image), np.amin(hsv_image))
@@ -53,19 +53,13 @@ def two_streams_rgb():
     test_datagen = ImageDataGenerator(rescale=1. / 255,
                                       preprocessing_function=color_transformation)
 
-    train_generator = train_datagen.flow_from_directory(
-        dataset_train,
-        target_size=(input_size, input_size),
-        classes=classes_train[:3])
+    train_generator = train_datagen.flow_from_directory(dataset_train,
+                                                        target_size=(input_size, input_size),
+                                                        classes=classes_train[:3])
 
-    train_generator[0] = [rgb2pca(x) for x in train_generator[0]]
-
-    validation_generator = test_datagen.flow_from_directory(
-        dataset_test,
-        target_size=(input_size, input_size),
-        classes=classes_test[:3])
-
-    validation_generator[0] = [rgb2pca(x) for x in validation_generator[0]]
+    validation_generator = test_datagen.flow_from_directory(dataset_test,
+                                                            target_size=(input_size, input_size),
+                                                            classes=classes_test[:3])
 
     output, im_input, input_shape = two_streams()
 
@@ -75,15 +69,13 @@ def two_streams_rgb():
     print(model.summary())
 
     opt = optimizers.SGD(lr=0.01, decay=0.0005, momentum=0.9)
-    model.compile(optimizer=opt,
-                  loss='categorical_crossentropy')
+    model.compile(optimizer=opt, loss='categorical_crossentropy')
 
-    model.fit_generator(
-        train_generator,
-        steps_per_epoch=20,
-        epochs=50,
-        validation_data=validation_generator,
-        validation_steps=80)
+    model.fit_generator(train_generator,
+                        steps_per_epoch=20,
+                        epochs=50,
+                        validation_steps=validation_generator[0].count() / validation_generator.batch_size,
+                        validation_data=validation_generator)
 
     # Save model and weights
     if not isdir(save_dir):
