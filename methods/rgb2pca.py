70fbd8169bdaa9b6f7cd48fd8d94c2122faefa5b
nilaisr <nilaisr@gmail.com>
2019-07-23 10:46:56 +0200

    test

--- methods/rgb2pca.py
+++ methods/rgb2pca.py
@@ -5,4 +5,4 @@ import numpy as np
 def rgb2pca(x):
     # The following matrix are the exact PCs from the Imagenet 2012 pixels color distribution.
     m = np.array([[0.65, 1.02, -0.49], [0.67, 0.004, 1], [0.67, -0.98, -0.51]])
-    return np.dot(x, m)
+    return np.dot(x, m, casting="unsafe")
