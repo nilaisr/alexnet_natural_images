62ebb9c94190aa6c55d980ea1864e57a5163ad44
nilaisr <nilaisr@gmail.com>
2019-08-10 18:09:09 +0200

    fix transf

--- methods/rgb2pca.py
+++ methods/rgb2pca.py
@@ -5,9 +5,10 @@ import numpy as np
 def rgb2pca(x):
     # The following matrix are the exact PCs from the Imagenet 2012 pixels color distribution.
     m = np.array([[0.65, 1.02, -0.49], [0.67, 0.004, 1], [0.67, -0.98, -0.51]])
-    x_pca = np.zeros((224, 224, 3))
-    for i in range(x.shape[0]):
-        for k in range(m.shape[0]):
-            for j in range(x.shape[1]):
-                x_pca[i, j, :] = sum(x[i, j, :] * m[k, :])
+    # x_pca = np.zeros((224, 224, 3))
+    # for i in range(x.shape[0]):
+    #     for k in range(m.shape[0]):
+    #         for j in range(x.shape[1]):
+    #             x_pca[i, j, :] = sum(x[i, j, :] * m[k, :])
+    x_pca = np.dot(x[:, :, :3], m)
     return x_pca
